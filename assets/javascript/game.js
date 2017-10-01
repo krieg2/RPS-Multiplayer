225a354144523134d22ef17a2f4e0805b80fa4ad
krieg2 <aaronk1@yahoo.com>
2017-09-30 22:49:19 -0400

    initial layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCGedS-eXOY3alApgtMk_cWyFlaxtsEuwA",
+    authDomain: "rockpaper-611cc.firebaseapp.com",
+    databaseURL: "https://rockpaper-611cc.firebaseio.com",
+    projectId: "rockpaper-611cc",
+    storageBucket: "",
+    messagingSenderId: "284046562557"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
